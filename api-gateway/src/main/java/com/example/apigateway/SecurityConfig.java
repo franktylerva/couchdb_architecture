6d3bcf5b986e6b020ddd746563617d1cb7d72f37
ftyler <ftyler.ctr@kr.af.mil>
2021-03-05 14:48:44 -0500

    Added oauth2 authentication with keycloak.

--- api-gateway/src/main/java/com/example/apigateway/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/SecurityConfig.java
@@ -2,34 +2,26 @@ package com.example.apigateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public MapReactiveUserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("password")
-            .roles("USER")
-            .build();
-        return new MapReactiveUserDetailsService(user);
-    }
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
+        return http
             .csrf().disable()
             .authorizeExchange()
-                .anyExchange().authenticated()
-                .and()
-            .httpBasic().and()
-            .formLogin();
-        return http.build();
+            .anyExchange().authenticated()
+            .and()
+            .oauth2Login()
+            .and()
+            .build();
     }
+
 }
